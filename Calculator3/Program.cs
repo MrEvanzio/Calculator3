fb233dba68be68bf98b284561b905d9a43a58842
MrEvanzio <MrEvanzio@users.noreply.github.com>
2019-04-27 20:17:39 -0400

    Se agrega codigo de prueba

--- Calculator3/Program.cs
+++ Calculator3/Program.cs
@@ -10,7 +10,16 @@ namespace Calculator3
     {
         static void Main(string[] args)
         {
-           
+            int suma;
+
+            Console.WriteLine("Escriba 2 Numeros a sumar: ");
+            Console.WriteLine("Escriba 1er Numero : ");
+            suma = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escriba 2do Numero : ");
+            suma = suma + Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("La suma de ambos numeros es : " + suma);
+
         }
     }
 }
