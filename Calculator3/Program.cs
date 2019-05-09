75fb3b6725717849033a34cc95bcc87acd57cd19
MrEvanzio <MrEvanzio@users.noreply.github.com>
2019-05-09 11:23:19 -0400

    Segunda Sincronizacion (aun no terminado)

--- Calculator3/Program.cs
+++ Calculator3/Program.cs
@@ -10,16 +10,48 @@ namespace Calculator3
     {
         static void Main(string[] args)
         {
-            int suma;
+            string input;
+            string multNumber1 = "";
+            string multNumber2 = "";
+            int tempResul = 0;
+            int count = 0;
 
-            Console.WriteLine("Escriba 2 Numeros a sumar: ");
-            Console.WriteLine("Escriba 1er Numero : ");
-            suma = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Escriba 2do Numero : ");
-            suma = suma + Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("La suma de ambos numeros es : " + suma);
+            Console.WriteLine("Escriba numero a calcular (use +, -, * o / ): ");
+            Console.WriteLine("Escriba (Ej. '10 + 5 * 2') : ");
+            input = Console.ReadLine();
 
+
+            //eliminar los espacios
+            Console.WriteLine(input);
+            input = input.Replace(" ", "");
+            int cantChar = input.Length;
+            //checkear por parentesis
+            //Exponentes
+            //Multiplicaciones y Divisiones
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '*')
+                {
+                    int cant = i - 1;
+                    while (Char.IsDigit(input[cant]))
+                    {
+                        Console.WriteLine(input[cant]);
+                        multNumber1 = Convert.ToString(input[cant]) + multNumber1;
+                        cant--;
+                    }
+                    cant = i + 1;
+                    while (Char.IsDigit(input[cant]))
+                    {
+                        Console.WriteLine(input[cant]);
+                        multNumber2 = Convert.ToString(input[cant]) + multNumber2;
+                        cant++;
+                    }
+                }
+            }
+
+            Console.WriteLine(multNumber1 + " + " + multNumber2);
+            //Sumas y Restas
         }
     }
 }
